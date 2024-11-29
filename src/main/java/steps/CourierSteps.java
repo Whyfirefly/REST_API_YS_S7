7bf52f0c1a2c2cd17fecd84705781c61d602c81c
Yana Shmygleva <shmyaniv@mail.ru>
2024-11-29 15:35:33 +0300

    changes from 29.11 : add Request specification from RestAssured (Base uri)

--- src/main/java/steps/CourierSteps.java
+++ src/main/java/steps/CourierSteps.java
@@ -1,20 +1,32 @@
 package steps;
 
+import constants.RandomData;
+import constants.Urls;
 import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import pojo.CreateCourier;
 import pojo.LoginCourier;
 
+import static constants.RandomData.RANDOM_ID;
 import static constants.Urls.*;
 import static io.restassured.RestAssured.given;
 
 public class CourierSteps {
 
+  //Base uri присваиваем через request specification в RestAssured
+  public static RequestSpecification requestSpecification() {
+    return given().log().all()
+            .contentType(ContentType.JSON)
+            .baseUri(Urls.BASE_URL);
+  }
+
   @Step("Создание курьера")
+
   public Response createCourier(String login, String pass, String name) {
     CreateCourier courier = new CreateCourier(login, pass, name);
-    return given()
-            .header("Content-type", "application/json")
+    return requestSpecification()
             .body(courier)
             .when()
             .post(COURIER_POST_CREATE);
@@ -23,8 +35,7 @@ public class CourierSteps {
   @Step("Логин курьера в системе")
   public Response loginCourier(String login, String pass) {
     LoginCourier loginCourier = new LoginCourier(login, pass);
-    return given()
-            .header("Content-type", "application/json")
+    return requestSpecification()
             .body(loginCourier)
             .when()
             .post(COURIER_POST_LOGIN);
@@ -40,9 +51,15 @@ public class CourierSteps {
   @Step("Удаление курьера")
   public Response deleteCourier(String login, String pass) {
 
-    return given()
-            .header("Content-type", "application/json")
+    return requestSpecification()
             .when()
             .delete(COURIER_DELETE+(getCourierId(login, pass)));
+  }
+
+  @Step("Получение рандомного id курьера")
+  public Response setRandomCourierId(String id) {
+    return requestSpecification()
+            .when()
+            .post(COURIER_DELETE+RANDOM_ID);
   }
 }
--- src/main/java/steps/OrderSteps.java
+++ src/main/java/steps/OrderSteps.java
@@ -1,29 +1,41 @@
 package steps;
 
+import constants.Urls;
 import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import pojo.OrderCreate;
+
+import java.io.File;
 
 import static constants.Urls.*;
 import static io.restassured.RestAssured.given;
 
-
 public class OrderSteps {
-  @Step("Создание нового заказа с использованием {order}")
-  public Response createOrder(OrderCreate order) {
-    return given()
-            .header("Content-type", "application/json")
+
+  //Base uri присваиваем через request specification в RestAssured
+  public static RequestSpecification requestSpecification() {
+    return given().log().all()
+            .contentType(ContentType.JSON)
+            .baseUri(Urls.BASE_URL);
+  }
+
+  @Step("Создание нового заказа с использованием {order} _ прописываем строку")
+  public Response createOrderWithLine(OrderCreate order) {
+    return requestSpecification()
             .body(order)
             .when()
             .post(ORDER_POST_CREATE);
   }
 
-  @Step("Получение списка заказов")
-  public Response getOrdersList() {
-    return given()
-            .header("Content-type", "application/json")
-            .when()
-            .get(ORDER_GET_LIST);
+  @Step("Создание заказа из JSON файла")
+  public Response createNewOrderFromJsonInFile(){
+    File json = new File("src/test/resources/newOrders.json");
+          return requestSpecification()
+                    .and()
+                    .body(json)
+                    .when()
+                    .post(ORDER_POST_CREATE);
   }
-
 }
