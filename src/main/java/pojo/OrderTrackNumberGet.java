f73ade289376a096ae74f8331e506a2aeba0ef0f
Yana Shmygleva <shmyaniv@mail.ru>
2024-12-03 13:32:46 +0300

    changes from 03.12 : OrderTrackNumberGet

--- src/main/java/pojo/OrderTrackNumberGet.java
+++ src/main/java/pojo/OrderTrackNumberGet.java
@@ -6,20 +6,20 @@ public class OrderTrackNumberGet {
   //Трекинговый номер заказа
   private int trackOrderNumber;
 
-  public int getTrackOrderNumber() {
-    return trackOrderNumber;
-  }
-
-  public void setTrackOrderNumber(int t) {
-    this.trackOrderNumber = trackOrderNumber;
-  }
-
   public OrderTrackNumberGet(int t) {
     this.trackOrderNumber = t;
   }
 
   public OrderTrackNumberGet( ) {
 
+  }
+
+  public int getTrackOrderNumber() {
+    return trackOrderNumber;
+  }
+
+  public void setTrackOrderNumber(int t) {
+    this.trackOrderNumber = trackOrderNumber;
   }
 
 
